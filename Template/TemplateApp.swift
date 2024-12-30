b1d6511fdfc8cb887d982568e2830222740ed637
Eugene Honcharenko <tulusheg@gmail.com>
2024-12-30 10:53:47 +0100

    Initial Commit

---
+++ Template/TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateApp.swift
+//  Template
+//
+//  Created by Eugene Honcharenko on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
